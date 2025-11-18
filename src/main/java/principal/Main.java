25552a1cca7b683e969c28e54e28c089ae7a64c6
Ziehaus <tarazonam706@gmail.com>
2025-11-18 09:03:54 -0500

    upload

---
+++ src/main/java/principal/Main.java
@@ -0,0 +1,15 @@
+
+package principal;
+
+import java.util.Map;
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, Modelo.IEscena> escenas = Modelo.CargadorEscenas.cargar();
+
+        Modelo.Juego juego = new Modelo.Juego(escenas, "escena_inicio");
+
+        new Vista.VentanaPrincipal(juego);
+    }
+}
+
